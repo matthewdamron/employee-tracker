2157f709ceef15fcaa456b437acd7ec49bdd4457
Matthew Damron <mattdamron@msn.com>
2021-02-05 16:52:56 -0700

    add starter prompts

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const inguirer = require('inquirer');
 const mysql2 = require('mysql2');
 const consoleTable = require('console.table');
+const inquirer = require('inquirer');
 
 // Create mysql connection
 const connection = mysql2.createConnection ({
@@ -15,7 +16,53 @@ const connection = mysql2.createConnection ({
 connection.connect(function(err) {
     if (err) throw err;
     console.log('Connected as id ' + connection.threadId + '\n');
-    // startApp();
-
-    connection.end();
+    startApp();
 });
+
+function startApp() {
+    inquirer.prompt ([
+        {
+            type: 'list',
+            message: 'What would you like to do?',
+            name: 'choice',
+            choices: [
+                'View',
+                'Add',
+                'Update',
+                'Remove'
+            ]
+        }
+    ]).then(function(res) {
+        switch (res.choice) {
+            case 'View':
+                inquirer.prompt ([
+                    {
+                        type: 'list',
+                        message: 'How would you like to view the employees?',
+                        name: 'choice',
+                        choices: [
+                            'View All Employees',
+                            `View Employee's By Deparment`,
+                            `View Employee's By Manager`,
+                            `View Employee's By Role`,
+                            'Back'
+                        ]
+                    }
+                ]).then(function(res) {
+                    switch (res.choice) {
+                        case value:
+                            
+                            break;
+                    
+                        default:
+                            break;
+                    }
+                })
+                
+                break;
+        
+            default:
+                break;
+        }
+    })
+};
